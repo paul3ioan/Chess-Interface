3176256dc69c9fe6ac596eec0e10d45b3cfc190b
Paul Cirstean <paul33ioan@gmail.com>
2021-07-09 01:54:33 +0300

    load position

--- Project4/Source.cpp
+++ Project4/Source.cpp
@@ -2,23 +2,24 @@
 #include <time.h>
 //#include "Connector.hpp"
 using namespace sf;
-
+#include <fstream>
 int size = 56;
 Vector2f offset(28, 28);
 
 Sprite f[32]; //figures
-std::string position = "";
-    
-int board[8][8] =
-{ -1,-2,-3,-4,-5,-3,-2,-1,
- -6,-6,-6,-6,-6,-6,-6,-6,
-  0, 0, 0, 0, 0, 0, 0, 0,
-  0, 0, 0, 0, 0, 0, 0, 0,
-  0, 0, 0, 0, 0, 0, 0, 0,
-  0, 0, 0, 0, 0, 0, 0, 0,
-  6, 6, 6, 6, 6, 6, 6, 6,
-  1, 2, 3, 4, 5, 3, 2, 1 };
+void loadPosition();
 
+int board[8][8] =
+{
+     0, 0, 0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0, 0, 0,
+};
 std::string toChessNote(Vector2f p)
 {
     std::string s = "";
@@ -29,6 +30,7 @@ std::string toChessNote(Vector2f p)
 
 Vector2f toCoord(char a, char b)
 {
+    // de ce?
     int x = int(a) - 97;
     int y = 7 - int(b) + 49;
     return Vector2f(x * size, y * size);
@@ -46,52 +48,33 @@ void move(std::string str)
         if (f[i].getPosition() == oldPos) f[i].setPosition(newPos);
 
     //castling       //if the king didn't move
-    if (str == "e1g1") if (position.find("e1") == -1) move("h1f1");
-    if (str == "e8g8") if (position.find("e8") == -1) move("h8f8");
-    if (str == "e1c1") if (position.find("e1") == -1) move("a1d1");
-    if (str == "e8c8") if (position.find("e8") == -1) move("a8d8");
+    if (str == "e1g1") move("h1f1");
+    if (str == "e8g8") move("h8f8");
+    if (str == "e1c1") move("a1d1");
+    if (str == "e8c8") move("a8d8");
 }
-
-void loadPosition()
-{
-    int k = 0;
-    for (int i = 0;i < 8;i++)
-        for (int j = 0;j < 8;j++)
-        {
-            int n = board[i][j];
-            if (!n) continue;
-            int x = abs(n) - 1;
-            int y = n > 0 ? 1 : 0;
-            f[k].setTextureRect(IntRect(size * x, size * y, size, size));
-            f[k].setPosition(size * j, size * i);
-            k++;
-        }
-
-    for (int i = 0;i < position.length();i += 5)
-        move(position.substr(i, 4));
-}
-
 
 int main()
 {
-    RenderWindow window(VideoMode(504, 504), "The Chess! (press SPACE)");
+    //504
+    RenderWindow window(VideoMode(1000, 1000), "The Chess! (press SPACE)");
     //    ConnectToEngine("stockfish.exe");    
 
     Texture t1, t2;
     t1.loadFromFile("images/figure.png");
-    t2.loadFromFile("images/board.png");
-
+    t2.loadFromFile("images/94205495-seamless-modern-black-and-white-chess-board-background-design-vector-illustration-eps10.jpg");
+    //cautat alte imagini
     for (int i = 0;i < 32;i++) f[i].setTexture(t1);
     Sprite sBoard(t2);
 
     loadPosition();
-    
+
     bool isMove = false;
     float dx = 0, dy = 0;
     Vector2f oldPos, newPos;
     std::string str;
     int n = 0;
-
+    // cum functioneaza toata partea asta
     while (window.isOpen())
     {
         Vector2i pos = Mouse::getPosition(window) - Vector2i(offset);
@@ -103,11 +86,11 @@ int main()
                 window.close();
 
             ////move back//////
-            if (e.type == Event::KeyPressed)
-                if (e.key.code == Keyboard::BackSpace)
-                {
-                    if (position.length() > 6) position.erase(position.length() - 6, 5); loadPosition();
-                }
+            //if (e.type == Event::KeyPressed)
+              //  if (e.key.code == Keyboard::BackSpace)
+                //{
+                  //  if (position.length() > 6) position.erase(position.length() - 6, 5); loadPosition();
+                //}
 
             /////drag and drop///////
             if (e.type == Event::MouseButtonPressed)
@@ -129,7 +112,7 @@ int main()
                     newPos = Vector2f(size * int(p.x / size), size * int(p.y / size));
                     str = toChessNote(oldPos) + toChessNote(newPos);
                     move(str);
-                    if (oldPos != newPos) position += str + " ";
+                    // if (oldPos != newPos) position += str + " ";
                     f[n].setPosition(newPos);
                 }
         }
@@ -156,7 +139,7 @@ int main()
                 window.display();
             }
 
-            move(str);  position += str + " ";
+            move(str);  //position += str + " ";
             f[n].setPosition(newPos);
         }
 
@@ -171,7 +154,85 @@ int main()
         window.display();
     }
 
-    //CloseConnection();
+    
 
     return 0;
+}
+void loadPosition()
+{
+    // loadpositionul e gata cred
+    std::ifstream cin("Text.txt");
+    std::string position, moveList;
+    cin >> position;
+
+
+    int line = 0, col = 0;
+    for (char c : position)
+    {
+        if (c <= '9' and c >= '0')
+        {
+            col += c - '0';
+        }
+        else
+            if (c == '/')
+                line++, col = 0;
+            else
+            {
+                switch (c)
+                {
+                case 'r':
+                    board[line][col] = -1;
+                    break;
+                case 'b':
+                    board[line][col] = -3;
+                    break;
+                case 'k':
+                    board[line][col] = -5;
+                    break;
+                case 'q':
+                    board[line][col] = -4;
+                    break;
+                case 'p':
+                    board[line][col] = -6;
+                    break;
+                case 'n':
+                    board[line][col] = -2;
+                    break;
+                case 'R':
+                    board[line][col] = 1;
+                    break;
+                case 'N':
+                    board[line][col] = 2;
+                    break;
+                case 'B':
+                    board[line][col] = 3;
+                    break;
+                case 'Q':
+                    board[line][col] = 4;
+                    break;
+                case 'K':
+                    board[line][col] = 5;
+                    break;
+                case 'P':
+                    board[line][col] = 6;
+                    break;
+                }
+                col++;
+            }
+    }
+    cin.close();
+    int k = 0;
+    for (int i = 0;i < 8;i++)
+        for (int j = 0;j < 8;j++)
+        {
+            int n = board[i][j];
+            if (!n) continue;
+            int x = abs(n) - 1;
+            int y = n > 0 ? 1 : 0;
+            f[k].setTextureRect(IntRect(size * x, size * y, size, size));
+            f[k].setPosition(size * j, size * i);
+            k++;
+        }
+
+    
 }
